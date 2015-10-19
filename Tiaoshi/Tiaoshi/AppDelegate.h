811ed1af1ecd01084f45ec76efbb311b9fc94227
Yanhu <yanyouchi@163.com>
2015-10-19 11:55:40 +0800

    tianjiatiaoshi

---
+++ Tiaoshi/Tiaoshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tiaoshi
+//
+//  Created by Mr.Yan on 15/10/12.
+//  Copyright © 2015年 Mr.Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

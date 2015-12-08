fb302d546ff9059dbf4ee30b6453989013e711d4
Yanhu <yanyouchi@163.com>
2015-12-08 16:58:49 +0800

    豆果美食框架

---
+++ Douguomeishi/iOSFinancial/MainViewController/FirstViewController.m
@@ -0,0 +1,87 @@
+//
+//  FirstViewController.m
+//  CommonProject
+//
+//  Created by Mr.Yang on 15/10/19.
+//  Copyright © 2015年 Mr.Yang. All rights reserved.
+//
+
+#import "FirstViewController.h"
+#import "CustomInvestGuideViewController.h"
+#import "CloudTabbarController.h"
+
+@interface FirstViewController ()
+
+@property (nonatomic) UITapGestureRecognizer *tap;
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar setBackgroundImage:HTImage(@"FirstHead") forBarMetrics:UIBarMetricsDefault];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    __weakSelf;
+    [self setImage:HTImage(@"FirstImage") WithTouchBlock:^(NSIndexPath *indexPath) {
+        //  结算页面
+        BaseDetailViewController *detail3 = [[BaseDetailViewController alloc] init];
+        detail3.isHidnBar = YES;
+        [weakSelf.navigationController.navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+        [detail3 setImage:HTImage(@"xuanzeriqi") WithTouchBlock:^(NSIndexPath *indexPath) {
+            
+            BaseDetailViewController *detail4 = [[BaseDetailViewController alloc] init];
+            detail4.isHidnBar = YES;
+            __weak BaseDetailViewController *weakBase = detail4;
+            [detail4 setImage:HTImage(@"jilutizhong") WithTouchBlock:^(NSIndexPath *indexPath) {
+                
+                if (weakBase.isOpen) {
+                    CloudTabbarController *VC = [[CloudTabbarController alloc]init];
+                    [VC showPromptView];
+                    [weakSelf.navigationController presentViewController:VC animated:YES completion:nil];
+
+                }else
+                {
+                    BaseDetailViewController *detail5 = [[BaseDetailViewController alloc] init];
+                    detail5.title = @"你购物我包邮";
+                    
+                    [detail5 setImage:HTImage(@"nigouwuwobaoyou") WithTouchBlock:^(NSIndexPath *indexPath) {
+                        
+                        
+                        
+                        BaseDetailViewController *detail9 = [[BaseDetailViewController alloc] init];
+                        detail9.title = @"绑定验证";
+                        
+                        [detail9 setImage:HTImage(@"kaihuxingmingyanzheng") WithTouchBlock:^(NSIndexPath *indexPath) {
+                            
+                            [weakSelf setAlerConten:@"恭喜，您的薄荷金融服务已开通，同时也已获得了1元现金，连续7天记录体重即可每天都获得1元现金哦~" withLeftBtn:@"我知道了" rightBtn:nil];
+                            [weakBase refreshView:HTImage(@"jilutizhongback")];
+                            weakBase.isOpen = YES;
+                        }];
+                        detail9.hidesBottomBarWhenPushed = YES;
+                        [weakSelf.navigationController pushViewController:detail9 animated:YES];
+                        
+                    }];
+                    detail5.hidesBottomBarWhenPushed = YES;
+                    [weakSelf.navigationController pushViewController:detail5 animated:YES];
+                }
+                
+                
+            }];
+            detail4.hidesBottomBarWhenPushed = YES;
+            [weakSelf.navigationController pushViewController:detail4 animated:NO];
+            
+        }];
+        detail3.hidesBottomBarWhenPushed = YES;
+        [weakSelf.navigationController pushViewController:detail3 animated:YES];    }];
+    
+}
+
+@end
+
+
---
+++ Douguomeishi/iOSFinancial/MainViewController/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  NoMoneyTravel
+//
+//  Created by Mr.Yang on 15/11/3.
+//  Copyright © 2015年 Mr.Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CustomInvestGuideViewController.h"
+#import "CloudTabbarController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar setBackgroundImage:HTImage(@"SecondHead") forBarMetrics:UIBarMetricsDefault];
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    __weakSelf;
+    [self setImage:HTImage(@"SecondImage") WithTouchBlock:^(NSIndexPath *indexPath) {
+        
+            
+            BaseDetailViewController *detail4 = [[BaseDetailViewController alloc] init];
+            detail4.title = @"钻石兑换";
+            __weak BaseDetailViewController *weakBase = detail4;
+            [detail4 setImage:HTImage(@"zuanshiduihuan") WithTouchBlock:^(NSIndexPath *indexPath) {
+                
+                BaseDetailViewController *detail5 = [[BaseDetailViewController alloc] init];
+                detail5.title = @"薄荷金融钻石大放送";
+                [detail5 setImage:HTImage(@"dafangsong") WithTouchBlock:^(NSIndexPath *indexPath) {
+                    
+                    CloudTabbarController *VC = [[CloudTabbarController alloc]init];
+                    [VC changeMessageWith:@"恭喜您已获得200个薄荷钻石，快去看看吧"];
+                    VC.selectedIndex = 1;
+                    [weakSelf.navigationController presentViewController:VC animated:YES completion:nil];
+                    [weakSelf.navigationController popToViewController:weakSelf.navigationController.viewControllers[1] animated:NO];
+                    [weakBase refreshView:HTImage(@"zuanshiduihuanback")];
+                    
+                }];
+                detail5.hidesBottomBarWhenPushed = YES;
+                [weakSelf.navigationController pushViewController:detail5 animated:YES];
+
+                
+            }];
+            detail4.hidesBottomBarWhenPushed = YES;
+            [weakSelf.navigationController pushViewController:detail4 animated:YES];
+            
+        }];
+    
+    
+    
+}
+
+@end
